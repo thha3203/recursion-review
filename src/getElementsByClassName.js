b0e29c170d4e217723622665643b9a7af7cffc59
Thanh Ha <thanh3203@gmail.com>
2021-11-08 17:39:08 -0800

    Assignment Complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // result
+  var result = [];
+
+  // innerFunction
+  var innerFunction = function(element) {
+    // iterate through the element
+    var children = element.childNodes;
+    children.forEach(function(child) {
+      if (child.classList && child.classList.contains(className)) {
+        result.push(child);
+      }
+      if (child.childNodes) {
+        innerFunction(child);
+      }
+    });
+  };
+
+  // invoke innerFunction
+  innerFunction(document);
+
+  // return result
+  console.log(result);
+  return result;
 };
