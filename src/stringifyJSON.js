1eab3b40a5c4a9a9dd99559037e8653f0ef472ce
Thanh Ha <thanh3203@gmail.com>
2021-11-08 17:39:39 -0800

    Identified Base Case

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,25 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let result = '';
+  // Base case
+  if (typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === 'string') {
+    result += obj;
+  }
+
+  if (obj === null || obj === undefined || Number.isNaN(obj) === true || typeof obj === 'function') {
+    result += null;
+  }
+
+  // If obj is an Array, iterate through Array with forEach loop
+  if (Array.isArray(obj)) {
+    let resultArray = []
+
+
+    result += resultArray.join();
+  }
+
+  // If obj is an Object, iterate through Object with for in loop
+
+  return result;
 };
